6de4e0e0017e46a3bfdc9646a1a1e58c254f32a1
Rob Clarke <kumo@kumo.it>
2021-01-12 23:06:28 +0100

    First tests

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_id_from_url_extraction_short() {
+        let url = "https://store.line.me/stickershop/product/12969848/ja";
+        let id = get_sticker_pack_id(url);
+
+        assert_eq!(id, "12969848");
+    }
+}
+
+fn get_sticker_pack_id(url: &str) -> &str {
+    todo!();
+}
+
 fn main() {
     let args: Vec<String> = std::env::args().collect();
 

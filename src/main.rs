e788209b193c87fa83dd23c677afe026b431ca19
Rob Clarke <kumo@kumo.it>
2021-01-12 22:26:29 +0100

    Checking args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = std::env::args().collect();
+
+    if let Some(link) = args.get(1) {
+        println!("Link given: {}", link);
+    } else {
+        println!("No link given.");
+    }
 }

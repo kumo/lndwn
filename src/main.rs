ad1cc91a40b3a8e8024f444fb0dc52f939d7c3b5
Rob Clarke <kumo@kumo.it>
2021-01-13 10:25:51 +0100

    Adding missing tests

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,43 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_id_from_url_extraction_short() {
+    fn test_id_from_long_store_url() {
         let url = "https://store.line.me/stickershop/product/12969848/ja";
         let id = get_sticker_pack_id(url);
 
         assert_eq!(id, Some("12969848"));
+    }
+
+    #[test]
+    fn test_id_from_short_store_url() {
+        let url = "line://shop/detail/12969848";
+        let id = get_sticker_pack_id(url);
+
+        assert_eq!(id, Some("12969848"));
+    }
+
+    #[test]
+    fn test_id_from_share_link_url() {
+        let url = "https://line.me/S/sticker/12969848?lang=en&ref=lsh_stickerDetail";
+        let id = get_sticker_pack_id(url);
+
+        assert_eq!(id, Some("12969848"));
+    }
+
+    #[test]
+    fn test_youtube_url() {
+        let url = "https://www.youtube.com/watch?v=e_V9RBcgwHI";
+        let id = get_sticker_pack_id(url);
+
+        assert_eq!(id, None);
+    }
+
+    #[test]
+    fn test_line_url() {
+        let url = "http://line-stickers.com";
+        let id = get_sticker_pack_id(url);
+
+        assert_eq!(id, None);
     }
 }
 

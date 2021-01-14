88893d5bbc9379ec80fd26b1668d0aa31e883c62
Rob Clarke <kumo@kumo.it>
2021-01-14 17:16:41 +0100

    Simplifying the main function with a simple struct

--- src/main.rs
+++ src/main.rs
@@ -55,48 +55,59 @@ fn get_sticker_pack_id(url: &str) -> Option<&str> {
         .as_str())
 }
 
+/*fn get_sticker_pack_link(id: &str) -> Option<&str> {
+    let link = format!("http://dl.stickershop.line.naver.jp/products/0/0/1/{}/iphone/stickers@2x.zip", id).as_str();
+
+    return Some(link);
+}*/
+
+struct StickerPack {
+	name: String,
+}
+
+fn get_sticker_pack(link: &str) -> Option<StickerPack> {
+    let id = get_sticker_pack_id(link).expect("Couldn't find ID in link");
+
+    let link = format!("http://dl.stickershop.line.naver.jp/products/0/0/1/{}/iphone/stickers@2x.zip", id);
+
+    println!("Download link: {}", link);
+
+    let mut resp = reqwest::blocking::get(&link).unwrap();
+
+    if resp.status().is_success() {
+        println!("The request was successful!");
+        println!("Response: {:?}", resp);
+
+        let mut buf: Vec<u8> = vec!();
+        resp.copy_to(&mut buf).unwrap();
+
+        let reader = Cursor::new(buf);
+
+        let mut archive = zip::ZipArchive::new(reader).unwrap();
+
+        let mut file = archive.by_name("productInfo.meta").unwrap();
+        let mut file_contents = String::new();
+        file.read_to_string(&mut file_contents).unwrap();
+
+        println!("{:?}", file_contents);
+
+        let meta = json::parse(&file_contents).unwrap();
+
+        println!("Title is {}", meta["title"]["en"]);
+        println!("Rest is {}", meta);
+
+        Some(StickerPack { name: meta["title"]["en"].to_string() })
+    } else {
+        None
+    }
+}
+
 fn main() {
     let args: Vec<String> = std::env::args().collect();
 
-    if let Some(link) = args.get(1) {
-        println!("Link given: {}", link);
+    let link = args.get(1).expect("You didn't specify a link");
 
-        if let Some(id) = get_sticker_pack_id(link) {
-            println!("ID found: {}", id);
+    let sticker_pack = get_sticker_pack(link).expect("Sticker pack not found");
 
-            let link = format!("http://dl.stickershop.line.naver.jp/products/0/0/1/{}/iphone/stickers@2x.zip", id);
-
-            println!("Download link: {}", link);
-
-            let mut resp = reqwest::blocking::get(&link).unwrap();
-
-            if resp.status().is_success() {
-                println!("The request was successful!");
-                println!("Response: {:?}", resp);
-
-                let mut buf: Vec<u8> = vec!();
-                resp.copy_to(&mut buf).unwrap();
-
-                let reader = Cursor::new(buf);
-
-                let mut archive = zip::ZipArchive::new(reader).unwrap();
-
-                let mut file = archive.by_name("productInfo.meta").unwrap();
-                let mut file_contents = String::new();
-                file.read_to_string(&mut file_contents).unwrap();
-
-                println!("{:?}", file_contents);
-
-                let meta = json::parse(&file_contents).unwrap();
-
-                println!("Title is {}", meta["title"]["en"]);
-            } else {
-                println!("Something else happened. Status: {:?}", resp.status());
-            }
-        } else {
-            println!("No ID found");
-        }
-    } else {
-        println!("No link given.");
-    }
+    println!("Sticker pack is {}", sticker_pack.name);
 }
